fb7dcd8b87bd565e18a1c94e1cdae569e06e3795
Brant Wynn <brant.wynn@acquia.com>
2016-10-19 18:20:08 -0500

    Add wrapper for moduleExists to ensure moduleList is up to date.

--- src/EventSubscriber/ConfigRewriteInstallSubscriber.php
+++ src/EventSubscriber/ConfigRewriteInstallSubscriber.php
@@ -3,6 +3,7 @@
 namespace Drupal\config_rewrite\EventSubscriber;
 
 use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\DrupalKernelInterface;
 use Drupal\Core\Extension\ModuleHandler;
 use Drupal\Core\Extension\Extension;
 use Drupal\Core\Extension\ModuleEvents;
@@ -35,15 +36,22 @@ class ConfigRewriteInstallSubscriber implements EventSubscriberInterface {
   protected $loggerFactory;
 
   /**
+   * @var \Drupal\Core\DrupalKernelInterface
+   */
+  protected $kernel;
+
+  /**
    * ConfigRewriteInstallSubscriber constructor.
    * @param \Drupal\Core\Extension\ModuleHandler $module_handler
    * @param \Drupal\Core\Config\ConfigFactory $config_factory
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   * @param \Drupal\Core\DrupalKernelInterface $kernel
    */
-  public function __construct(ModuleHandler $module_handler, ConfigFactory $config_factory, LoggerChannelFactoryInterface $logger_factory) {
+  public function __construct(ModuleHandler $module_handler, ConfigFactory $config_factory, LoggerChannelFactoryInterface $logger_factory, DrupalKernelInterface $kernel) {
     $this->moduleHandler = $module_handler;
     $this->configFactory = $config_factory;
     $this->loggerFactory = $logger_factory;
+    $this->kernel = $kernel;
   }
 
   /**
@@ -58,10 +66,23 @@ class ConfigRewriteInstallSubscriber implements EventSubscriberInterface {
    * @param \Symfony\Component\EventDispatcher\Event $event
    */
   public function module_install_config_rewrite(Event $event) {
+    // Check that the module is enabled.
     $module = $event->getModule();
-    if ($this->moduleHandler->moduleExists($module)) {
+    if ($this->moduleExists($module)) {
       $this->rewriteModuleConfig($this->moduleHandler->getModule($module));
     }
+  }
+
+  /**
+   * @param $module
+   * @return bool
+   */
+  public function moduleExists($module) {
+    if (!$this->moduleHandler->moduleExists($module)) {
+      $container = $this->kernel->getContainer();
+      $this->moduleHandler = $container->get('module_handler');
+    }
+    return $this->moduleHandler->moduleExists($module);
   }
 
   /**
